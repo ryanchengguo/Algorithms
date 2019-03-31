25e5b7dc98d8a6d64e7d03e13a7f60ebc3863ceb
Ryan Guo <guohao_lin@msn.com>
2019-03-30 17:06:09 -0700

    88_MergeSortedArray

---
+++ Array/88_MergeSortedArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        while( n > 0 ){
+            if(m==0 || nums2[n-1] > nums1[m-1]){
+                nums1[m+n-1] = nums2[--n];
+            }else{
+                nums1[m+n-1] = nums1[--m];
+            }
+        }
+    }
+}
+
+//m==0 solve problem java.lang.ArrayIndexOutOfBoundsException: -1 at line 4
+//time complexity O(n^2) space O(n^2)
