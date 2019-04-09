503e47f31fdd4378623ca177e44c42a717ec0c5a
Ryan Guo <guohao_lin@msn.com>
2019-04-08 23:58:26 -0700

    Update B160_Intersection of Two Linked Lists.java

--- Linked List/B160_Intersection of Two Linked Lists.java
+++ Linked List/B160_Intersection of Two Linked Lists.java
@@ -2,7 +2,17 @@ class Solution{
 	public LinkNode getIntersectionNode(ListNode headA, ListNode headB){
 		HashSet<LinkNode> visited = new HashSet<>();
 		HashNode p1 = headA, p2 = headB;
-		
+		while(p1 != null){
+			visited.add(p1);
+			p1 = p1.next;
+		}
+		while(p2 != null){
+			if(visited.contains(p2)){
+				return p2;
+			}
+			p2 = p2.next;	
+		}
+		return null;
 	}
 }
 
